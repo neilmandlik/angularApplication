bbddac1be4161260ee272bb8ac12780bc30d5fa7
neilmandlik <nrm110604@gmail.com>
2025-08-28 01:43:04 +0530

    Importing Roles in the app component for nesting

--- frontend/first-ng-app/src/app/app.ts
+++ frontend/first-ng-app/src/app/app.ts
@@ -1,15 +1,22 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Roles } from './components/roles/roles';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Roles],
   template: `
     <h1>Welcome to {{ title() }}!</h1>
-
+    <p>Hello World</p>
+    <app-roles />
     <router-outlet />
   `,
-  styles: [],
+  styles: [`
+    p {
+      font-family: Lato;
+      background-color: lightblue;
+    } 
+    `],
 })
 export class App {
   protected readonly title = signal('first-ng-app');
