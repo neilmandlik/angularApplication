56d7599033b4989f8ddfd25345fbf87b102ff2f0
neilmandlik <nrm110604@gmail.com>
2025-08-28 18:46:16 +0530

    Making the designation API Call and presenting it in a tabular format

--- frontend/first-ng-app/src/app/components/designation/designation.ts
+++ frontend/first-ng-app/src/app/components/designation/designation.ts
@@ -1,23 +1,25 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, inject, OnInit, ChangeDetectorRef } from '@angular/core';
 import { MasterService } from '../../services/master';
-
+import { IAPIDesignationsResponse, IDesignations } from '../../model/interface/roles';
 @Component({
   selector: 'app-designation',
   imports: [],
-  template: `
-    <p>
-      designation works!
-    </p>
-  `,
+  templateUrl: './designations.html',
   styles: ``
 })
 export class Designation implements OnInit {
 
   ms = inject(MasterService)
+  cdr = inject(ChangeDetectorRef)
+
+  designationList:IDesignations[]=[]
+  isLoading: boolean = true;
 
   ngOnInit(): void {
-    this.ms.getDesignations().subscribe(res=>{
-      console.log(res)
+    this.ms.getDesignations().subscribe((res: IAPIDesignationsResponse)=>{
+      this.designationList = res.data;
+      this.isLoading = false;
+      this.cdr.detectChanges();
     })
   }
 
