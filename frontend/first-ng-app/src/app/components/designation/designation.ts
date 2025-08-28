330b1399ed140c1ff813d064ad147476f776b6b1
neilmandlik <nrm110604@gmail.com>
2025-08-28 11:29:48 +0530

    Adding a designation child component

---
+++ frontend/first-ng-app/src/app/components/designation/designation.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-designation',
+  imports: [],
+  template: `
+    <p>
+      designation works!
+    </p>
+  `,
+  styles: ``
+})
+export class Designation {
+
+}
