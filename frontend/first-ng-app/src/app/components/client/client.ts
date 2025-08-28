6be94f5d1ab5a65e9519a3e7095e3719b1ce868e
neilmandlik <nrm110604@gmail.com>
2025-08-28 18:45:30 +0530

    Adding two more components

---
+++ frontend/first-ng-app/src/app/components/client/client.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-client',
+  imports: [],
+  template: `
+    <p>
+      client works!
+    </p>
+  `,
+  styles: ``
+})
+export class Client {
+
+}
---
+++ frontend/first-ng-app/src/app/components/employee/employee.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-employee',
+  imports: [],
+  template: `
+    <p>
+      employee works!
+    </p>
+  `,
+  styles: ``
+})
+export class Employee {
+
+}
