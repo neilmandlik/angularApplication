5e59d084ddb5b6435fb7e89c10de321fd7d712d9
neilmandlik <nrm110604@gmail.com>
2025-08-29 13:12:58 +0530

    Adding a get all routes function

--- backend/controllers/adminControllers.js
+++ backend/controllers/adminControllers.js
@@ -1,5 +1,6 @@
 const roles = require('../models/roles.json'); // Adjust path relative to controllers folder
 const desgn = require('../models/designations.json');
+const clients = require('../models/clients.json');
 
 const getRoles = (req, res) => {
   res.json(roles);
@@ -8,4 +9,18 @@ const getRoles = (req, res) => {
 const getDesignations = (req, res) => {
   res.json(desgn)
 };
-module.exports = { getRoles, getDesignations };
+
+const getClients = (req, res) => {
+  // Implementation for getting clients
+  res.json(clients);
+}
+
+const addUpdateClient = (req, res) => {
+  // Implementation for adding or updating a client
+  res.send('Client added/updated');
+};
+module.exports = { 
+  getRoles, 
+  getDesignations, 
+  addUpdateClient,
+  getClients};
--- backend/middleware/routes/adminRoutes.js
+++ backend/middleware/routes/adminRoutes.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const router = express.Router();
-const { getRoles, getDesignations } = require('../../controllers/adminControllers');
+const { 
+    getRoles,
+    getDesignations,
+    addUpdateClient,
+    getClients
+} = require('../../controllers/adminControllers');
 
 router.get('/roles', getRoles);
 router.get('/designations',getDesignations);
+router.get('/clients', getClients);
+router.post('/client', addUpdateClient);
 
 module.exports = router;
