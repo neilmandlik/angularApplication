b4711677620c41a77ddae6d7ec15cc0b0e502b47
neilmandlik <nrm110604@gmail.com>
2025-08-31 21:20:31 +0530

    Importing Constant and Writing Route Names from the Constants object

--- frontend/first-ng-app/src/app/services/client.ts
+++ frontend/first-ng-app/src/app/services/client.ts
@@ -5,6 +5,7 @@ import { IAPIClientsResponse, IAPIEmployeesResponse, IAPIClientProjectsResponse 
 import { environment } from '../../environments/environmentWithAPI';
 import { Clientclass } from '../model/class/Client';
 import { ClientProjectclass } from '../model/class/ClientProject';
+import { Constant } from '../constant/Constant';
 
 @Injectable({
   providedIn: 'root'
@@ -13,28 +14,28 @@ export class ClientService {
   http = inject(HttpClient);
 
   addClientProject(obj: ClientProjectclass){
-    return this.http.post<ClientProjectclass[]>(environment.API_URL+'clientProjects',obj)
+    return this.http.post<ClientProjectclass[]>(environment.API_URL+Constant.API_METHOD.CRUD_CLIENT_PROJECTS,obj)
   }
 
   getAllClientProjects(){
     console.log(environment.API_URL+'clientProjects')
-    return this.http.get<ClientProjectclass[]>(environment.API_URL+'clientProjects')
+    return this.http.get<ClientProjectclass[]>(environment.API_URL+Constant.API_METHOD.CRUD_CLIENT_PROJECTS)
   }
 
   getAllEmployees():Observable<IAPIEmployeesResponse>{
     console.log(environment.API_URL+'employees')
-    return this.http.get<IAPIEmployeesResponse>(environment.API_URL+'employees')
+    return this.http.get<IAPIEmployeesResponse>(environment.API_URL+Constant.API_METHOD.CRUD_EMPLOYEES)
   }
 
   getAllClients():Observable<IAPIClientsResponse>{
-    return this.http.get<IAPIClientsResponse>(environment.API_URL+'clients')
+    return this.http.get<IAPIClientsResponse>(environment.API_URL+Constant.API_METHOD.CRUD_CLIENTS)
   }
 
   addUpdateClients(obj:Clientclass):Observable<IAPIClientsResponse>{
-    return this.http.post<IAPIClientsResponse>(environment.API_URL+'addclient',obj)
+    return this.http.post<IAPIClientsResponse>(environment.API_URL+Constant.API_METHOD.CRUD_CLIENTS,obj)
   }
 
   deleteClients(cId:number):Observable<IAPIClientsResponse>{
-    return this.http.delete<IAPIClientsResponse>(environment.API_URL+'deleteclient/'+cId)
+    return this.http.delete<IAPIClientsResponse>(environment.API_URL+Constant.API_METHOD.CRUD_CLIENTS+`/${cId}`)
   }
 }
