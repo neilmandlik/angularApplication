c0b6a294274898e01bccce8d0e56fd93e00a6215
neilmandlik <neil.mandlik@imocha.co>
2025-09-04 11:54:43 +0530

    Changing the port number

--- backend/middleware/corsAuth/corsOpt.js
+++ backend/middleware/corsAuth/corsOpt.js
@@ -1,7 +1,7 @@
 const cors = require('cors');
 
 const corsOptions = {
-  origin: 'http://localhost:4200', // Allow requests from Angular app
+  origin: 'http://localhost:4201', // Allow requests from Angular app
   credentials: true                // If you need to send cookies/auth
 };
 
